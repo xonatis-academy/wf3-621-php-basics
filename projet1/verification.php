6057eb1a42628b3483b6ee1109d48dbc5fc5cc4f
xonatis-academy <formation@xonatis.com>
2021-02-02 17:39:32 +0100

    Adding service

--- projet1/verification.php
+++ projet1/verification.php
@@ -1,11 +1,30 @@
 <?php
 
+class SecurityService
+{
+    public function verifie(string $email, string $mdp)
+    {
+        // Si c'est bon, alors il retourne true
+        // Sinon, il retourne false
+        if ($email == 'admin@admin.com' && $mdp == 'admin')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
 $email = $_GET['email'];
 $motDePasse = $_GET['mdp'];
 
+$william = new SecurityService();
+$reponse = $william->verifie($email, $motDePasse);
 // si l'email est égal à admin@admin.com ET le mot de passe est égal à admin
 // ben la page est redirigée vers result.html
-if ($email == 'admin@admin.com' && $motDePasse == 'admin')
+if ($reponse == true)
 {
     header('LOCATION: result.html');
 }
