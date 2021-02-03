274774f40793d21c6511ed95a25561f9aad14a11
xonatis-academy <formation@xonatis.com>
2021-02-03 12:33:55 +0100

    Detailing projet1

---
+++ projet1/test.php
@@ -0,0 +1,14 @@
+<?php
+// Inclus dans le dossier courant, le fichier index.html.php
+// Crée une variable $email avec '' dedans
+// Crée une variable $mdp avec '' dedans
+// Si le paramètre email est dans l'URL
+    // Récupère le paramètre email dans l'URL et met le dans la variable $email
+// Si le paramètre mdp est dans l'URL
+    // Récupère le paramètre mdp dans l'URL et met le dans la variable $mdp
+// Si la variable $email est strictement égale à admin@admin.com et que la variable 
+// $mdp est strictement égale à admin
+    // Dis au client de se rediriger vers le fichier result.html.php
+// Sinon
+    // Répond un code réponse 401
+?>
