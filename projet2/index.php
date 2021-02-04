9416f113a2d95a06c2d4ea113848ae8e867048b6
xonatis-academy <formation@xonatis.com>
2021-02-04 17:25:09 +0100

    Adding SESSION to store $tableau

--- projet2/index.php
+++ projet2/index.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 class ArticleSelectionne
 {
     public $nom;
@@ -9,6 +11,10 @@ class ArticleSelectionne
 $total = 0;
 
 $tableau = [];
+if (isset($_SESSION['panier']))
+{
+    $tableau = $_SESSION['panier'];
+}
 
 if (isset($_GET['btn-moto']))
 // Si le paramètre 'btn-moto' est dans l'URL 
@@ -19,6 +25,36 @@ if (isset($_GET['btn-moto']))
     // On renseigne les champs pour 1 moto
     $article->nom = 'Moto';
     $article->prix = 1234.50;
+    $article->quantite = 1;
+
+    // On ajoute l'article dans le tableau des articles
+    $tableau[] = $article;
+}
+
+if (isset($_GET['btn-livre']))
+// Si le paramètre 'btn-moto' est dans l'URL 
+{
+    // On crée un nouvel article vide
+    $article = new ArticleSelectionne();
+
+    // On renseigne les champs pour 1 moto
+    $article->nom = 'Livre';
+    $article->prix = 250.50;
+    $article->quantite = 1;
+
+    // On ajoute l'article dans le tableau des articles
+    $tableau[] = $article;
+}
+
+if (isset($_GET['btn-voiture']))
+// Si le paramètre 'btn-moto' est dans l'URL 
+{
+    // On crée un nouvel article vide
+    $article = new ArticleSelectionne();
+
+    // On renseigne les champs pour 1 moto
+    $article->nom = 'Voiture de ouf';
+    $article->prix = 5050.50;
     $article->quantite = 1;
 
     // On ajoute l'article dans le tableau des articles
@@ -38,6 +74,8 @@ for ($i = 0; $i < count($tableau); ++$i)
     // 3. On accumule le prix total de l'article dans la variable $total
     $total = $total + $prix_total_du_produit;
 }
+
+$_SESSION['panier'] = $tableau;
 
 include __DIR__.'/index.html.php';
 
